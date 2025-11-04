a92b142346524e320abaee6ec5a861f87a7293f1
Jason Garber <jason.garber@cargosense.com>
2025-11-04 12:45:28 -0500

    Update documenation URLs

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 /**
  * Import package using its full name and configured default export.
  *
- * @see {@link https://nodejs.org/dist/latest-v22.x/docs/api/packages.html#package-entry-points}
- * @see {@link https://nodejs.org/dist/latest-v22.x/docs/api/packages.html#self-referencing-a-package-using-its-name}
+ * @see {@link https://nodejs.org/dist/latest-v24.x/docs/api/packages.html#package-entry-points}
+ * @see {@link https://nodejs.org/dist/latest-v24.x/docs/api/packages.html#self-referencing-a-package-using-its-name}
  */
 export { default } from "@cargosense/eslint-config";
